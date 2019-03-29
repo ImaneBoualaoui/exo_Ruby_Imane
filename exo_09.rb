364d11f44341087d8cad221f211f7c79062f48ee
Imane <imane.92@live.fr>
2019-03-29 18:48:19 +0100

    Bonjour prenom + nom

---
+++ exo_09.rb
@@ -0,0 +1,9 @@
+puts "Quel est ton prénom ?"
+print ">"
+user_FirstName = gets.chomp
+
+puts "Quel est ton nom ?"
+print ">"
+user_name = gets.chomp
+
+puts "Bonjour, " + user_FirstName + " "+ user_name + " !"
